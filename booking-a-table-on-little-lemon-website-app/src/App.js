2d65bbe9dc1c724482226686d0e1250f288268a5
Tatenda_Mhaka <tatendamh@afrosoft.co.zw>
2024-01-05 11:23:53 +0200

    Booking a table reserve website

--- booking-a-table-on-little-lemon-website-app/src/App.js
+++ booking-a-table-on-little-lemon-website-app/src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+// or import { Suspense } from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Loading from "./pages/Loading";
+const LazySharedPage = React.lazy(() => import("./pages/SharedPage"));
+const LazyHome = React.lazy(() => import("./pages/Home"));
+const LazyAbout = React.lazy(() => import("./pages/About"));
+const LazyMenu = React.lazy(() => import("./pages/Menu"));
+const LazyReserve = React.lazy(() => import("./pages/Reserve"));
+const LazyError = React.lazy(() => import("./pages/Error"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HashRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <React.Suspense fallback={<Loading />}>
+                <LazySharedPage />
+              </React.Suspense>
+            }
+          >
+            {/* the index will always match the path="/" */}
+            <Route
+              index
+              element={
+                <React.Suspense fallback={<Loading />}>
+                  <LazyHome />
+                </React.Suspense>
+              }
+            />
+            <Route
+              path="about"
+              element={
+                <React.Suspense fallback={<Loading />}>
+                  <LazyAbout />
+                </React.Suspense>
+              }
+            />
+            <Route
+              path="menu"
+              element={
+                <React.Suspense fallback={<Loading />}>
+                  <LazyMenu />
+                </React.Suspense>
+              }
+            />
+            <Route
+              path="reserve"
+              element={
+                <React.Suspense fallback={<Loading />}>
+                  <LazyReserve />
+                </React.Suspense>
+              }
+            />
+            <Route
+              path="*"
+              element={
+                <React.Suspense fallback={<Loading />}>
+                  <LazyError />
+                </React.Suspense>
+              }
+            />
+          </Route>
+        </Routes>
+      </HashRouter>
+    </>
   );
 }
 
